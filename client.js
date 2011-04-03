fcc7369ecf48257259b06d0f4eb23b63bab10535
Logan Barnett <logustus@gmail.com>
2011-04-03 08:43:40 -0700

    Fixed wrap-around bug with layout. Changed title to match app name.

--- client.js
+++ client.js
@@ -388,9 +388,9 @@ function showChat (nick) {
 //we want to show a count of unread messages when the window does not have focus
 function updateTitle(){
   if (CONFIG.unread) {
-    document.title = "(" + CONFIG.unread.toString() + ") node chat";
+    document.title = "(" + CONFIG.unread.toString() + ") Catch-A-Captcha";
   } else {
-    document.title = "node chat";
+    document.title = "Catch-A-Captcha";
   }
 }
 
